5539c96a950e00916c9d86870ea3cf1f8f4eed9c
webVueBlog <2979986590@qq.com>
2022-02-21 16:20:28 +0800

    add: 实现文件的切割

--- vue-upload-big-file/src/App.vue
+++ vue-upload-big-file/src/App.vue
@@ -1,17 +1,67 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+	  <input type="file" @change="handleFileChange" />
+	  <el-button @click="handleUpload">上传</el-button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+const Status = {
+	wait: "wait",
+	pause: "pause",
+	uploading: "uploading"
+}
+const SIZE = 0.5*1024*1024;
 
 export default {
   name: 'App',
+  data: () => ({
+	  container: { // 将我们的任务放到一起
+		  file: null,
+		  hash: "", // 哈希
+	  },
+	  status: Status.waiting
+  }),
+ //  data() {
+	// return() {
+		
+	// }  
+ //  },
+  methods: {
+	  async handleUpload(e) {
+		  // 大量的任务
+		  if (!this.container.file) return;
+		  this.status = Status.uploading;
+		  // 获取切片
+		  const fileChunkList = this.createFileChunk(this.container.file);
+		  // 点击上传的时候，状态为上传状态
+		  console.log(fileChunkList, 'fileChunkList');
+		  // 0:
+		  // file: Blob {size: 69306, type: ''}
+		  // [[Prototype]]: Object
+		  // length: 1
+	  },
+	  // es6可少传参数
+	  createFileChunk(file, size = SIZE) {
+		  const fileChunkList = []; // 切片的数组
+		  let cur = 0;
+		  while(cur < file.size) {
+			  fileChunkList.push({
+				  file: file.slice(cur, cur + size)
+			  })
+			  cur += size;
+		  }
+		  return fileChunkList;
+	  },
+	  handleFileChange(e) {
+		  // 分割文件
+		  const [file] = e.target.files; // 拿到第一个文件
+		  // console.log(e.target.files);
+		  this.container.file = file;
+	  }
+  },
   components: {
-    HelloWorld
+	  
   }
 }
 </script>
